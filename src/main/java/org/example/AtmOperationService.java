01254a94ab718f5cd64738a052b8d3489f6fbd0a
IMOBI LIBERTY <107262741+engineerLiberty@users.noreply.github.com>
2023-03-25 13:10:43 +0100

    Removed some comments

--- src/main/java/org/example/AtmOperationService.java
+++ src/main/java/org/example/AtmOperationService.java
@@ -6,10 +6,3 @@ public interface AtmOperationService {
     public void depositAmount(double depositAmount);
     public void withdrawAmount(double withdrawAmount);
 }
-//echo "# Atm_Machine_Operation" >> README.md
-//        git init
-//        git add README.md
-//        git commit -m "first commit"
-//        git branch -M main
-//        git remote add origin git@github.com:engineerLiberty/Atm_Machine_Operation.git
-//        git push -u origin main
